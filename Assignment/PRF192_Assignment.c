da6c646ec915ec25115e82b2f40cb3194e5e9647
Harding Adonis <hardingadonis@gmail.com>
2022-10-27 16:08:49 +0700

    Update Assignment

---
+++ Assignment/PRF192_Assignment.c
@@ -0,0 +1,19 @@
+// ASSIGNMENT
+
+// Subject: PRF192
+// Class:   SE17C03
+
+// Members of Group 1:
+// - Lê Minh Vương - QE170148
+// - Đinh Quốc Chương - QE170097
+// - Nguyễn Thị Thúy - QE170033
+// - Hồ Trọng Nghĩa - QE170173
+
+// Topic 1: Students in a class
+
+#include <stdio.h>
+
+int main()
+{
+	return 0;
+}
